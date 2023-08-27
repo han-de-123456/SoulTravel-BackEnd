05bf3db8d3bc9a6d4a4cc3643265bc968621fe70
e <2862530972@qq.com>
2023-08-27 21:38:17 +0800

    项目搭建

--- soultravel-user/src/main/java/org/soul/SoulTravelUserApplication.java
+++ soultravel-user/src/main/java/org/soul/SoulTravelUserApplication.java
@@ -1,8 +1,10 @@
 package org.soul;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("org.soul.mapper")
 @SpringBootApplication
 public class SoulTravelUserApplication {
     public static void main(String[] args) {
